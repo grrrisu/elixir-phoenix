9157d6ba3ab3f113b951c8c70fd69875a6c59639
Alessandro Di Maria <adm@m42.ch>
2017-03-27 22:26:38 +0200

    list topics

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -3,6 +3,11 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render conn, "index.html", topics: topics
+  end
+
   def new(conn, _params) do
     changeset = Topic.changeset( %Topic{} )
     render conn, "new.html", changeset: changeset
@@ -12,8 +17,15 @@ defmodule Discuss.TopicController do
     changeset = Topic.changeset( %Topic{}, topic )
 
     case Repo.insert(changeset) do
-      {:ok, _topic} -> render conn, "index"
-      {:error, changeset} -> render conn, "new.html", changeset: changeset
+      {:ok, _topic} ->
+        conn
+        |> put_flash("Topic created", :info)
+        |> redirect(to: topic_path(conn, :index))
+
+      {:error, changeset} ->
+        conn
+        |> put_flash("Failed to create topic", :error)
+        |> render("new.html", changeset: changeset)
     end
   end
 
