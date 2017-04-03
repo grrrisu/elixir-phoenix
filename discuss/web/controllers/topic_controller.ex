2bd5d5cc3479c5b7e19a16837b0ba07fc0634ae1
Alessandro Di Maria <adm@m42.ch>
2017-04-03 22:08:31 +0200

    edit topic

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -29,4 +29,27 @@ defmodule Discuss.TopicController do
     end
   end
 
+  def edit(conn, %{"id" => id}) do
+    topic = Repo.get(Topic, id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, %{"id" => id, "topic" => topic_params}) do
+    topic     = Repo.get(Topic, id)
+    changeset = Topic.changeset(topic, topic_params)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash("Topic updated", :info)
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> put_flash("Failed to update topic", :error)
+        |> render("edit.html", changeset: changeset, topic: topic)
+    end
+  end
+
 end
