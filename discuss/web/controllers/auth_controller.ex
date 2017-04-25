4969d7b20b16b764dfa3aa51661c85c0dca8be3c
Alessandro Di Maria <adm@m42.ch>
2017-04-25 23:30:52 +0200

    current_user, signin and signout links

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -15,6 +15,13 @@ defmodule Discuss.AuthController do
     signin(conn, changeset)
   end
 
+  def signout(conn, _params) do
+    conn
+    |> configure_session(drop: true)
+    |> put_flash(:info, "Goodbye")
+    |> redirect(to: topic_path(conn, :index))
+  end
+
   defp signin(conn, changeset) do
     case get_or_create(changeset) do
       {:ok, user} ->
